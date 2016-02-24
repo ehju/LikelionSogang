857d2d979716d9d69c300759f9daef44d9d3dd08
Hello15369 <junejc0613@naver.com>
2016-02-24 18:09:32 +0000

    fix all

---
+++ public/js/typing_effect.js
@@ -0,0 +1,7 @@
+$(function(){
+  $(".intro-text").typed({ 
+    strings: ['<p class="intro-lead-in" style="font-style:normal;">모집합니다</p><br><p class="intro-heading intro-text2" style="margin-bottom: 25px; font-family:"asmileM";">멋쟁이 사자처럼 <span style="color:#FFD216">4기</span></p><br> <p class="intro-lead-in" style="margin-bottom:50px;">@서강대학교</p>'],
+    typeSpeed: 100,
+    contentType: 'html'
+  });
+});
