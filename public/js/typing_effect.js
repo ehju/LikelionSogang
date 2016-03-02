3460684a25f984702d94efa8e6c3ea90ae35d8d9
Hello15369 <junejc0613@naver.com>
2016-03-02 10:45:45 +0000

    intro img change

--- public/js/typing_effect.js
+++ public/js/typing_effect.js
@@ -1,6 +1,6 @@
 $(function(){
   $(".intro-text").typed({ 
-    strings: ['<p class="intro-lead-in" style="font-style:normal;">모집합니다</p><br><p class="intro-heading intro-text2" style="margin-bottom: 25px; font-family:"asmileM";">멋쟁이 사자처럼 <span style="color:#FFD216">4기</span></p><br> <p class="intro-lead-in" style="margin-bottom:50px;">@서강대학교</p>'],
+    strings: ['<p class="intro-lead-in" style="font-style:normal;">모집합니다 3/2 ~ 3/11</p><br><p class="intro-heading intro-text2" style="margin-bottom: 25px; font-family:"asmileM";">멋쟁이 사자처럼 <span style="color:#FFD216">4기</span></p><br> <p class="intro-lead-in" style="margin-bottom:50px;">@서강대학교</p>'],
     typeSpeed: 100,
     contentType: 'html'
   });
